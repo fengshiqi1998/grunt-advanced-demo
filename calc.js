a661489c3b9496b56e8a994947d273bc8761e933
fengshiqi1998 <512653808@qq.com>
2019-03-22 16:41:27 +0800

    mocha-fe

---
+++ calc.js
@@ -0,0 +1,11 @@
+/* exported Rectangle */
+function Rectangle(width, height) {
+  return {
+    'perimeter': function(width, height) {// 周长
+      return 2*(Number(width) + Number(height));
+    },
+    'area': function(width, height) {
+      return Number(width)*Number(height);              
+    }      
+  }; 
+}
